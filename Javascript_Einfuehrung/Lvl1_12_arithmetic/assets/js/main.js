060dd5e7b8ff4697f1f3cd159f8cc9fa1805ab7c
LanaxCode <123948215+LanaxCode@users.noreply.github.com>
2023-02-16 15:18:38 +0100

    Javascript_einfuehrung

---
+++ Javascript_Einfuehrung/Lvl1_12_arithmetic/assets/js/main.js
@@ -0,0 +1,28 @@
+// let score = 5 + 5 * 10;
+// console.log('Ergebnis: ' + score);
+
+// let score = (5 + 5) * 10;
+// console.log('Ergebnis: ' + score);
+// Du kannst Werte auf Variablen addieren.
+// let score = 0;
+// score = score + 10;
+// console.log('Ergebnis: ' + score);
+// Hiervon gibt es eine verkürzte Version.
+// let score = 0;
+// score += 10;
+// console.log('Ergebnis: ' + score);
+
+let zahl = 1;
+// zahl = zahl + 1;
+// zahl += 1;
+
+// Verwende Inkrement-Operatoren
+zahl++;
+console.log('increment: ' + zahl);
+
+// Verwende Dekrement-Operatoren
+zahl--;
+console.log('dekrement: ' + zahl);
+
+
+
---
+++ Javascript_Einfuehrung/Lvl1_1_console/assets/js/main.js
@@ -0,0 +1,24 @@
+let a = 12
+let b = a *3
+console.log(b)
+
+document.write(b) 
+
+
+console.log('Hello World')
+
+console.log ('anton')
+
+let firstName ="Anton"
+console.log (firstName)
+
+let Age = 28
+console.log (Age)
+
+let Job ="Trainer"
+console.log (Job)
+
+let Married = true
+console.log (Married)
+
+console.log (firstName + " ist " + Age + " Jahre alt, von Beruf " + Job +" und ist verheiratet: " + Married)
